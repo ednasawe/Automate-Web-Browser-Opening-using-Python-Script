909d0f6377392e23d9e374a4f1998891e1dfb6d4
Edna <cedna198@gmail.com>
2021-10-02 13:57:10 -0500

    UPDATE PYTHON SCRIPT
    updated python script

--- automatevisitors.py
+++ automatevisitors.py
@@ -6,8 +6,7 @@ import time
 import webbrowser
 
 count = 0
-urls = ['https://www.youtube.com/watch?v=xmts0wsSShU', 'https://www.youtube.com/watch?v=xhYlCQ9P94Q', 'https://techauthority.tech/2021/10/01/how-to-increase-youtube-views-using-python-script-programming-language-a-youtube-view-bot/', 'https://techauthority.tech/2021/09/18/apples-iphone-13-mini-iphone-13-iphone-13-pro-and-iphone-pro-max-tech-specs-and-new-features/', 'https://www.youtube.com/watch?v=RxriFUt5Fn0', ' https://www.youtube.com/watch?v=azC58dOm2cU', 'https://techauthority.tech/top-aeronautics-opportunities-from-nasa/', 'https://techauthority.tech/2019/08/30/learn-analytics-with-free-online-courses-by-google/']
-
+urls = ['INSERT_URL_HERE']
 
 while count < 100:
     for url in urls:
